5637224521916a50373c74ffc9d16f0bf17a9a80
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-04-05 18:28:52 -0600

    Created movie-view.jsx, main-view.jsx, movie-card.jsx and fixed an error in index.jsx

---
+++ src/components/book-view/movie-view.jsx
@@ -0,0 +1,32 @@
+import { MovieCard } from "../movie-card/movie-card";
+
+export const MovieView = ({ movie }) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.image} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.Description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genre}</span>
+      </div>
+      <div>
+        <span>Image: </span>
+        <span>{movie.image}</span>
+      </div>
+      <button>Back</button>
+    </div>
+  );
+};
---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { MovieCard } from "./components/movie-card/movie-card.jsx";
+import { MovieView } from "./components/movie-view/movie-view.jsx";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: "Eloquent JavaScript",
+      image: "https://image.jpg",
+      director: "Kevin Bacon",
+    },
+    {
+      id: 2,
+      title: "Mastering JavaScript Functional Programming",
+      image: "https://image.jpg",
+      director: "Kevin Bacon",
+    },
+    {
+      id: 3,
+      title: "JavaScript: The Good Parts",
+      image: "https://image.jpg",
+      director: "Kevin Bacon",
+    },
+    {
+      id: 4,
+      title: "JavaScript: The Definitive Guide",
+      image: "https://image.jpg",
+      director: "Kevin Bacon",
+    },
+    {
+      id: 5,
+      title: "The Road to React",
+      image: "https://image.jpg",
+      director: "Kevin Bacon",
+    },
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return (
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
+    );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
