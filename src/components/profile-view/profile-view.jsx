7b8197e67ccd395cdabca12c4f253f9936a47c21
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-05-12 11:03:15 -0600

    Fixed errors with updating profile data

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,23 +4,24 @@ import { MainView } from "../main-view/main-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { UpdateUser } from "./update-user";
 import { FavoriteMovies } from "./favorite-movies";
+//import Button from "react-bootstrap/Button";
+//import Form from "react-bootstrap/Form";
 
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button, Form } from "react-bootstrap";
 
 export const ProfileView = ({ localUser, movies }) => {
   console.log(movies);
   const storedUser = JSON.parse(localStorage.getItem("user"));
   //const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  
-  const [username, setUsername] = useState(localUser.UserName);
+  const [username, setUsername] = useState(localUser.Username);
+  const [password, setPassword] = useState(localUser.Password);
   const [email, setEmail] = useState(localUser.Email);
   const [birthday, setBirthday] = useState(localUser.Birthday);
-  const [password, setPassword] = useState("");
-const favs = movies.filter((movie) => {
-  return storedUser.FavoriteMovies.includes(movie.id);
-});
-console.log(movies);
+  const favs = movies.filter((movie) => {
+    return storedUser.FavoriteMovies.includes(movie.id);
+  });
+  console.log(movies);
   console.log(favs);
   /*const data = {
     Username: username,
@@ -28,14 +29,86 @@ console.log(movies);
     Email: email,
     Birthday: birthday,
   };*/
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-  
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday,
+    };
+    console.log(
+      "https://movies-api-qewk.onrender.com/users/" + localUser.Username
+    );
+    fetch("https://movies-api-qewk.onrender.com/users/" + localUser.Username, {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Authorizaton: "Bearer " + localStorage.getItem("token"),
+      },
+    }).then((response) => {
+      if (response.ok) {
+        alert("Update successful");
+        window.location.reload();
+      } else {
+        alert(JSON.stringify(response));
+      }
+    });
+  };
 
-return (
-  <>
-    <h2>User Profile</h2>
-    <h3>Login/Signup</h3>
-    <h3>Favorite Movies</h3>
-    {favs && favs.map((movie) => <MovieCard movie={movie} />)}
-  </>
-);
+  return (
+    <>
+      <h2>User Profile</h2>
+      <h3>Login/Signup</h3>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <h3>Favorite Movies</h3>
+      {favs && favs.map((movie) => <MovieCard movie={movie} />)}
+    </>
+  );
+};
